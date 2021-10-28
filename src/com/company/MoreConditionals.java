bbaac0d2163b883c67670739eac15bc1f2af3e98
student <ishaanpatel@billericak12.com>
2021-10-28 12:01:34 -0400

    Ocotober 28, 12:01

--- src/com/company/MoreConditionals.java
+++ src/com/company/MoreConditionals.java
@@ -1,48 +1,52 @@
 package com.company;
 
 public class MoreConditionals {
-    public static boolean isLeapYear(int year){
-        if (year % 400 == 0){
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
             return true;
-        }
-        else if (year % 100 == 0){
+        } else if (year % 100 == 0) {
             return false;
-        }
-        else if (year % 4 == 0){
+        } else if (year % 4 == 0) {
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
     public static boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2) {
         if (year1 == year2 && month1 == month2 && day1 == day2) {
             return false;
-        }
-        else if (year1 < year2) {
-            return false;
-        }
-        else if (year1 > year2) {
+        } else if (year1 != year2) {
+            return year1 > year2;
+        } else if (month1 != month2) {
+            return month1 > month2;
+        } else if (day1 != day2) {
+            return day1 > day2;
+        } else return false;
+    }
+
+    public static int findBestFit(int size1, int size2, int space) {
+        if (size1 > space && size2 > space) {
+            return 0;
+        } else if (size1 + size2 <= space) {
+            return 3;
+        } else if (size1 > space) {
+            return 2;
+        } else if (size2 > space) {
+            return 1;
+        } else if (size1 == size2) {
+            return 1;
+        } else if (size1 > size2 && size1 <= space) {
+            return 1;
+        } else return 2;
+    }
+
+    public static boolean makeBench(int small, int big, int goal) {
+        if (small + (5 * big) >= goal && goal % 5 <= small){
             return true;
-        }
-        else if (month1 < month2) {
-            return false;
-        }
-        else if (month1 > month2) {
-            return true;
-        }
-        else if (day1 > day2) {
-            return true;
-        }
-        else if (day1 < day2) {
-            return false;
         }
         else return false;
     }
-/*
-    public static int findBestFit(int size1, int size2, int space){
-        return true;
-    }
-*/
+
+
     public static void main(String[] args) {
         // leap year
         System.out.println("2000 " + isLeapYear(2000));
@@ -53,6 +57,7 @@ public class MoreConditionals {
 
 // is later
         System.out.println("1/2/2010 is later than 1/2/2011 " + isLater(1,2,2010, 1,2,2011));
+        System.out.println("2/1/2011 is later than 2/2/2010 " + isLater(2,1,2011, 2,2,2010));
         System.out.println("1/2/2011 is later than 2/2/2010 " + isLater(1,2,2011, 2,2,2010));
         System.out.println("1/5/2010 is later than 3/2/2010 " + isLater(1,5,2010, 3,2,2010));
         System.out.println("3/2/2010 is later than 1/2/2010 " + isLater(3,2,2010, 1,2,2010));
@@ -60,7 +65,7 @@ public class MoreConditionals {
         System.out.println("5/2/2010 is later than 1/3/2011 " + isLater(5,2,2010, 1,3,2011));
         System.out.println("1/2/2010 is later than 1/2/2010 " + isLater(1,2,2010, 1,2,2010));
         System.out.println("4/8/2010 is later than 1/2/2010 " + isLater(4,8,2010, 1,2,2010));
-/*
+
 // Best Fit
         System.out.println("Find Best fit 2 3 6 is " + findBestFit(2,3,6));
         System.out.println("Find Best fit 4 3 6 is " + findBestFit(4,3,6));
@@ -78,7 +83,6 @@ public class MoreConditionals {
         System.out.println("Make bench 3 4 20 is " + makeBench(3,4,20));
         System.out.println("Make bench 0 6 30 is " + makeBench(0,6,30));
         System.out.println("Make bench 0 5 30 is " + makeBench(0,5,30));
-        System.out.println("Make benches 2 6 23 is " + makeBenches(2,6,23));
-        */
+        System.out.println("Make benches 2 6 23 is " + makeBench(2,6,23));
     }
 }
